63037d55c86d2d1091fb12f67753084038782000
gaowei1012 <mingwei2222@126.com>
2020-12-06 17:01:03 +0800

    添加设置

--- src/views/personal/Setting.tsx
+++ src/views/personal/Setting.tsx
@@ -1,12 +1,20 @@
 import React from 'react'
-import { Text, SafeAreaView } from 'react-native'
+import { GoBack } from '../../utils/goback'
+import { SafeAreaView, Text } from 'react-native'
+import { NavigationBar } from '../../navigation/TopNavigationBar'
 import styles from '../../styles/setting'
 
 type settingType = {}
 
-const SettingPage: React.FC<settingType> = () => {
+const StatusBar = {
+    backgroundColor: '#ffffff',
+    barStyle: 'dark-content',
+}
+
+const SettingPage: React.FC<settingType> = (props) => {
     return (
         <SafeAreaView style={styles.settingContianer}>
+            <NavigationBar statusBar={StatusBar} leftButton={GoBack(props)} title="设置" />
             <Text>setting</Text>
         </SafeAreaView>
     )
