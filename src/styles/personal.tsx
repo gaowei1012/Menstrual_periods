f25262699ba9b74405daf4389376bff9c66de403
gaowei1012 <mingwei2222@126.com>
2020-12-06 11:28:15 +0800

    添加个人中心页面

--- src/styles/personal.tsx
+++ src/styles/personal.tsx
@@ -4,6 +4,39 @@ import { px2dp } from '../utils/px2dp'
 const styles = StyleSheet.create({
     personal: {
         flex: 1,
+        backgroundColor: '#f9f9f9',
+    },
+    headerBox: {
+        height: px2dp(176),
+        backgroundColor: '#7468BE',
+        borderBottomLeftRadius: px2dp(40),
+        borderBottomRightRadius: px2dp(40),
+    },
+    headerSetting: {
+        alignSelf: 'flex-end',
+        marginRight: px2dp(30),
+        marginTop: px2dp(5),
+    },
+    contentBox: {
+        width: px2dp(335),
+        height: px2dp(536),
+        alignSelf: 'center',
+        backgroundColor: '#fff',
+        borderRadius: px2dp(30),
+        position: 'relative',
+        top: px2dp(-123),
+    },
+    avatarBox: {
+        width: px2dp(100),
+        height: px2dp(100),
+        borderRadius: px2dp(50),
+        backgroundColor: 'red',
+        alignSelf: 'center',
+        marginTop: px2dp(23),
+    },
+    avatarName: {
+        marginTop: px2dp(10),
+        alignSelf: 'center',
     },
 })
 
--- src/views/personal/Personal.tsx
+++ src/views/personal/Personal.tsx
@@ -1,14 +1,30 @@
 import React from 'react'
 import styles from '../../styles/personal'
-import { SafeAreaView, Text } from 'react-native'
+import { SafeAreaView, Text, View, TouchableOpacity } from 'react-native'
 // import { Http } from '../../expand/axios'
 
 // const test_url = '/test'
 
+const header = () => (
+    <View style={styles.headerBox}>
+        <TouchableOpacity>
+            <Text style={styles.headerSetting}>设置</Text>
+        </TouchableOpacity>
+    </View>
+)
+
+const content = () => (
+    <View style={styles.contentBox}>
+        <View style={styles.avatarBox}></View>
+        <Text style={styles.avatarName}>name</Text>
+    </View>
+)
+
 const Personal = () => {
     return (
         <SafeAreaView style={styles.personal}>
-            <Text>about</Text>
+            {header()}
+            {content()}
         </SafeAreaView>
     )
 }
